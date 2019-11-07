96dc1a9e583b9ef3365e4820d577501c80a502c2
Anton <anton.pushkin2@gmail.com>
2019-11-07 21:02:57 +0300

    Lab 3 added Hashing function

---
+++ Lab_3/Lab_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Lab_3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = SimpleNumber(SimpleNumber(RandomNumber(100000000,999999999)));
+            Console.WriteLine(N);
+
+
+
+
+            Console.ReadLine();
+        }
+
+        public static string H(string data, string key)
+        {
+            byte[] data1 = Encoding.ASCII.GetBytes(data);
+            byte[] key1 = Encoding.ASCII.GetBytes(key);
+
+            using (var hmac = new HMACSHA1(key1))
+            {
+                return Encoding.ASCII.GetString(hmac.ComputeHash(data1));
+            }
+        }
+
+        string Salt(int length)
+        {
+            RNGCryptoServiceProvider p = new RNGCryptoServiceProvider();
+            var salt = new byte[length];
+            p.GetBytes(salt);
+            return Convert.ToBase64String(salt);
+        }
+
+        static int RandomNumber(int a, int b) 
+        {
+            Random rand = new Random();
+            return rand.Next(a,b);
+        }
+
+        static int SimpleNumber(int randnum)
+        {
+            return randnum * 2 + 1;
+        }
+    }
+}
